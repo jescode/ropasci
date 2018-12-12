82e38c00977a48d968803acc021f9a5c68404ac9
Jesko Steinberg <jesko.steinberg@ikor.de>
2018-12-12 22:32:10 +0100

    GameManager test logic boilerplate

---
+++ src/test/java/de/jescode/games/ropasci/logic/GameManagerTest.java
@@ -0,0 +1,46 @@
+package de.jescode.games.ropasci.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.jescode.games.ropasci.logic.entity.Player;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class GameManagerTest {
+	
+	//@Autowired
+	//GameManager gameManager;
+
+	@Test
+	@Ignore
+	public final void testSetPlayerNames() {
+//		final String nameOne = "Jesko";
+//		final String nameTwo = "Celina";
+//		final Player playerOne = gameManager.getPlayeOne();
+//		final Player playerTwo = gameManager.getPlayerTwo();
+//		gameManager.setPlayerNames(nameOne, nameTwo);
+//		
+//		assertEquals(playerOne.getName(), nameOne);
+//		assertEquals(playerTwo.getName(), nameTwo);
+	}
+
+	@Test
+	@Ignore
+	public final void testSetStrategy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Ignore
+	public final void testInitGameSettings() {
+		fail("Not yet implemented");
+	}
+
+}
